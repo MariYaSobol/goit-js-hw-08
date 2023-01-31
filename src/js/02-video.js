d1fd785f0a11dacb4bd0a4f6d0aee0b848fb9ac9
MariYaSobol <pasnova@ukr.net>
2023-02-01 01:26:03 +0300

    second

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY_LOCAL = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(KEY_LOCAL, data.seconds);
+}
+function currentTime() {
+  const time = localStorage.getItem(KEY_LOCAL);
+  if (time) {
+    player.setCurrentTime(time);
+  }
+}
+currentTime();
